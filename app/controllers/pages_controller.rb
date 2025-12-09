44239ff4461218c4bacb153b2d201b1b6c0a8b56
JosÃe Feliciano <jr.feliciano98@gmail.com>
2025-12-09 12:25:26 -0300

    commit na master home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    return unless user_signed_in?
+
+    if current_user.owner?
+      @my_restaurants = current_user.restaurants.limit(5)
+      @recent_ratings = Rating
+                          .where(restaurant: @my_restaurants)
+                          .order(created_at: :desc)
+                          .limit(10)
+    else # customer
+      @recent_restaurants = Restaurant.order(created_at: :desc).limit(5)
+      @recent_ratings = Rating.order(created_at: :desc).limit(10)
+    end
   end
 end
