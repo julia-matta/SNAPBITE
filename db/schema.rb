1e0e1c3c392cf88318feb705eaedd1d4f8ace9b6
andre rezende <andre_fmr89@hotmail.com>
2025-12-11 15:56:10 -0300

    pagina de perfil e de editar perfil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_10_193232) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_10_154607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
