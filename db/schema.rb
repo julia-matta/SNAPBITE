7fd768095245de3c7c0892a936dd1714138a225b
julia matta <juliamattaf@gmail.com>
2025-12-18 15:11:56 -0300

    add city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_16_190000) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_18_181052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -117,16 +117,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_16_190000) do
     t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
-  end
-
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "category"
@@ -139,6 +129,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_16_190000) do
     t.integer "price_range"
     t.integer "average_price"
     t.string "image_name"
+    t.string "city"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
